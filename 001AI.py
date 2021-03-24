5738c2daca74a2acdaa5c89d79230c4cb83138f0
Douglas Lima Benevides <51888076+douglaslimab@users.noreply.github.com>
2021-03-24 21:50:41 +0000

    numpy

--- 001AI.py
+++ 001AI.py
@@ -1,11 +1,32 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
-x2 = np.arange(1, 20)
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-y2 = np.random.random() * x2 + np.random.random()
-plt.plot(x2, y2)
-plt.plot(x2, y2, 'r--')
-# plt.plot(x1, y1, 'r', x2, y2, 'b--')
-plt.legend(['f1', 'f2'])
+arr3 = np.arange(0, 10)
+arr4 = np.arange(0, 10, 2)
+
+arr5 = np.zeros(5)
+
+arr6 = np.ones(4)
+arr7 = np.ones((3, 4))
+
+arr8 = np.eye(5)
+
+arr9 = np.linspace(0, 10, 3)
+arr10 = np.linspace(0, 10, 4)
+
+arr11 = np.random.rand(5)
+arr12 = np.random.rand(3, 4)
+
+arr13 = np.random.uniform(3, 7, 10)
+arr14 = np.random.uniform(3.5, 11.47, (3, 2))
+
+arr15 = np.random.randint(5, 13)
+arr16 = np.random.randint(5, 13, 7)
+arr17 = np.random.randint(5, 13, (3, 4))
+
+print(arr17)
+print(arr17.shape)
+print(arr17.reshape(2, 6))
+print(arr17)
