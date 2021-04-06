7446d2d996cb1ba5f8198b4382f8f5b0de0586cf
Douglas Lima Benevides <51888076+douglaslimab@users.noreply.github.com>
2021-04-06 19:10:13 +0100

    numpy

--- 010AI.py
+++ 010AI.py
@@ -2,9 +2,14 @@ import numpy as np
 
 a = np.array([[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14]])
 print(a)
+# get a specific element
 print(a[1, 5])
+# get specific row
 print(a[0, :])
+# get specific column
 print(a[:, 2])
+# get an interval in a row skipping 2
 print(a[0, 1:6:2])
+# get an interval in a row skipping -1
 print(a[1, 6:1:-1])
 print()
